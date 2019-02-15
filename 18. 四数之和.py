30685f4a04c946bcfed121f9e936fe997b0fc3ee
Alexkim <1034158844@qq.com>
2019-02-15 17:18:17 +0800

    Create 18. 四数之和.py

---
+++ 18. 四数之和.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    '''
+    题意：找到数列中所有和等于目标数的四元组，需去重
+    多枚举一个数后，参照3Sum的做法，O(N^3)    
+    '''
+    def fourSum(self, nums, target):
+        nums.sort()
+        res = []
+        length = len(nums)
+        for i in range(0, length - 3):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, length - 2):
+                if j != i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                sum = target - nums[i] - nums[j]
+                left, right = j + 1, length - 1
+                while left < right:
+                    if nums[left] + nums[right] == sum:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                        right -= 1
+                        left += 1
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+                    elif nums[left] + nums[right] > sum:
+                        right -= 1
+                    else:
+                        left += 1
+        return res
