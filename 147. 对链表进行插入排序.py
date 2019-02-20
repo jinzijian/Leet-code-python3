623fbcf5abf38a66716726a729691cdcd13f2ee8
Alexkim <1034158844@qq.com>
2019-02-20 16:50:06 +0800

    Update 147. 对链表进行插入排序.py

--- 147. 对链表进行插入排序.py
+++ 147. 对链表进行插入排序.py
@@ -15,9 +15,9 @@ class Solution(object):
         while cur.next:
             if cur.val > cur.next.val:#判断是否要进行插入操作
                 pre = dummy
-                while pre.next.val < cur.next.val:#循环用来让pre指到需要做交换操作的节点
+                while pre.next.val < cur.next.val:#循环用来让pre指到需要做交换操作的节点 
                     pre = pre.next
-                temp = cur.next
+                temp = cur.next               #cur是需要做插入的前一个指针 temp = cur.next是需要做插入操作的 pre是需要做交换操作的
                 cur.next = temp.next
                 temp.next = pre.next
                 pre.next = temp
