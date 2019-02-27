23da58f72abd21c602cc5849d660fbb585a6c1c6
Alexkim <1034158844@qq.com>
2019-02-27 16:19:38 +0800

    Update 144. 二叉树的前序遍历.py

--- 144. 二叉树的前序遍历.py
+++ 144. 二叉树的前序遍历.py
@@ -14,3 +14,27 @@ class Solution:
             self.preorderTraversal(root.right)
             
         return self.ret
+    
+#方法二 迭代算法实现---用栈
+class Solution:
+    
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = []
+        if root == None:
+            return result
+
+        stack = []
+        stack.append(root)
+        while(len(stack))!=0:
+            top = stack.pop()
+            if top.right!=None:
+                stack.append(top.right)
+            if top.left!=None:
+                stack.append(top.left)    
+            result.append(top.val)    
+        return result    
+            
