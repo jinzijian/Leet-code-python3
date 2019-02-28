0d3f19c770d2e055409c3a5f5006158ae34f2488
Alexkim <1034158844@qq.com>
2019-02-28 11:22:50 +0800

    Update 145. 二叉树的后序遍历.py

--- 145. 二叉树的后序遍历.py
+++ 145. 二叉树的后序遍历.py
@@ -9,3 +9,31 @@ class Solution(object):
         self.postorderTraversal(root.right)
         self.ret.append(root.val)
         return self.ret
+    
+    
+    
+    
+  #方法二 递归写法
+#思路：对于一个节点p，如果p没有左右孩子，我们直接cout p.val，如果p有左右孩子，我们分别加入栈中即可。 前序遍历的reverse
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result = list()
+        if root == None:
+            return result
+        
+        stack = list()
+        stack.append(root)
+        while len(stack) != 0:
+            top = stack.pop()
+            if top.left != None:
+                stack.append(top.left)
+            if top.right != None:
+                stack.append(top.right)
+
+            result.insert(0, top.val)#逆序操作
+                
+        return result  
