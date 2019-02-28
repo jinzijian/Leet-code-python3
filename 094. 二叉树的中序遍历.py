2867f9124dd06d72d11c5854a92a97ffe356c32f
Alexkim <1034158844@qq.com>
2019-02-28 11:02:54 +0800

    Update 094. 二叉树的中序遍历.py

--- 094. 二叉树的中序遍历.py
+++ 094. 二叉树的中序遍历.py
@@ -14,5 +14,24 @@ class Solution(object):
             self.ret.append(root.val)
             self.inorderTraversal(root.right)
         return self.ret
-        
+# 方法二：迭代算法
+#我们先把节点所有的左节点放入栈中，然后开始出栈，每次出栈都把栈中的元素放入到结果中，并且把
+#每个结果的右孩子放入栈中。因此，这里的遍历顺序就是左->中->右。
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack = []
+        res = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            root = stack.pop()
+            res.append(root.val)
+            root = root.right        
         
