f73e8571544eadb37d0056aed243530d78563fee
Alexkim <1034158844@qq.com>
2019-03-01 10:59:29 +0800

    Create 102. 二叉树的层次遍历.py

---
+++ 102. 二叉树的层次遍历.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue = [root]
+        res = []
+        while queue:
+            next_queue = []
+            res.append([node.val for node in queue])
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            queue = next_queue      
+        return res    
