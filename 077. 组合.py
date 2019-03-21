e3e6e54124cc8758b8b050ef6968422a7ec1a307
Alexkim <1034158844@qq.com>
2019-03-21 14:39:23 +0800

    Update 077. 组合.py

--- 077. 组合.py
+++ 077. 组合.py
@@ -15,3 +15,22 @@ class Solution(object):
         dfs(n,k,[],res,1)
         return res
                 
+#剪枝方法
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        def dfs(n,k,path,res,start):
+            if len(path) == k:
+                res.append(path)
+                return
+            for i in range(start,n+2-k+len(path)):
+                dfs(n,k,path+[i],res,i+1)
+        res = []
+        dfs(n,k,[],res,1)
+        return res
+                
+        
