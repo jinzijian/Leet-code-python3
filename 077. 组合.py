e0a3e3f4d0afa0556f4c38da442413ab7db0291f
Alexkim <1034158844@qq.com>
2019-03-21 14:32:59 +0800

    Create 077. 组合.py

---
+++ 077. 组合.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        def dfs(n,k,path,res,start):
+            if len(path) == k:
+                res.append(path)
+                return
+            for i in range(start,n+1):
+                dfs(n,k,path+[i],res,i+1#注意必须是i+1而不是start+1)
+        res = []
+        dfs(n,k,[],res,1)
+        return res
+                
