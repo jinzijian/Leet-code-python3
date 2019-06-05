07a4ab9259a2b94f8e967d805deadd62a931e5ee
Alexkim <1034158844@qq.com>
2019-06-05 16:39:38 +0800

    Update 242. 有效的字母异位词.py

--- 242. 有效的字母异位词.py
+++ 242. 有效的字母异位词.py
@@ -1,18 +1,19 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        record = {}
-        if len(s) < len(t):
-            a = s
-            s = t
-            t = a
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """ 
+        if len(s)!= len(t):
+            return False
+        used1 = {}
+        used2 = {}
         for i in s:
-            record[i] = record.setdefault(i,0) + 1
+            used1[i] = used1.get(i,0)+1#关键步骤如何赋值给字典
         for j in t:
-            if j in record:
-                record[j] -= 1
-                if record[j] == 0:
-                    record.pop(j)
-        if record =={}:
+            used2[j] = used2.get(j,0)+1
+        if used1 == used2:
             return True
         else:
             return False
