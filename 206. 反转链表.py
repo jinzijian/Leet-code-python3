b1cace4d7d0af625e83a178d16ede8f487c47dce
Alexkim <1034158844@qq.com>
2019-06-16 11:09:14 +0800

    Update 206. 反转链表.py

--- 206. 反转链表.py
+++ 206. 反转链表.py
@@ -1,13 +1,28 @@
-class Solution:
+#迭代做法
+class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
         pre = None
-        while(head!=None):
+        while head is not None:
             temp = head.next
             head.next = pre
             pre = head
             head = temp
         return pre
+    
+#递归解决
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        newhead = self.reverseList(head.next)#此时head.next为反转后链表的尾节点
+        head.next.next = head #把head接到head.next后面
+        head.next = None
+        return newhead
