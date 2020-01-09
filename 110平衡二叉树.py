ab7c72f5880e9186134f44c298539a53e30cd5ff
Alexkim <1034158844@qq.com>
2020-01-09 15:20:48 +0800

    Update 110平衡二叉树.py

--- 110平衡二叉树.py
+++ 110平衡二叉树.py
@@ -1,3 +1,28 @@
+#自顶部向下，相当于做了n次dfs所以o（n2）
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if root == None:
+            return True
+        if root.left is None and root.right is None:
+            return True
+        lb = self.isBalanced(root.left)
+        rb = self.isBalanced(root.right)
+        ld = self.maxdepth(root.left)
+        rd = self.maxdepth(root.right)
+        if lb and rb and abs(ld-rd)<=1:
+            return True
+        else:
+            return False
+    def maxdepth(self,root):
+        if root == None:
+            return 0
+        left = self.maxdepth(root.left)
+        right = self.maxdepth(root.right)
+        return max(left,right)+1
+
+
+
+
 class Solution(object):
     def isBalanced(self, root):
         """
